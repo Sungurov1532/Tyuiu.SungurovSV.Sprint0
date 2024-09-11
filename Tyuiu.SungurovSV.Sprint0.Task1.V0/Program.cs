86c80643e54bac065db1aca2f5e81de342ddae37
Tyuiu.SungurovSV@outlook.com <sungurovvladislav794@gmail.com>
2024-09-11 20:27:31 +0300

    Task1

---
+++ Tyuiu.SungurovSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.SungurovSV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Владик";
+            int age = 100;
+            Console.Write("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
