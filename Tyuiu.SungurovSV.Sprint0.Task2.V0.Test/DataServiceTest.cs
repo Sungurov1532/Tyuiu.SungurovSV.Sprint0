6d9747ce83e425d7f6561666edf75f04e21b40fa
Tyuiu.SungurovSV@outlook.com <sungurovvladislav794@gmail.com>
2024-09-19 11:20:41 +0300

    Task2

---
+++ Tyuiu.SungurovSV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SungurovSV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SungurovSV.Sprint0.Task2.V0.Test
+
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Игорь";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Игорь", res);
+        }
+    }
+}
